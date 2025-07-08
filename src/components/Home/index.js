040ed03df9f09e9a9a5d265bcd4e019840d29644
Kevin Zhu <kevinzhu35@gmail.com>
2025-07-07 22:51:56 -0700

    fixed random gap on mobile with contact form + other pages
    went through all other routing pages to change position of page div to relative to give height to entire body. the problem lies in previous hard-coded heights and margins that affected later spacing issues

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,26 +4,29 @@ import Projects from "../Projects";
 
 const Home = () => {
   return (
-    <div className="page-container home-page">
-      <div className="text-zone">
-        <h1>
-          <span>
-            Hi!
-            <br />
-            I'm <Link className="name" to="/about">Kevin</Link>
-            <br />
-            <span className="title">Full-Stack Developer</span>
-          </span>
-        </h1>
-        <h2>
-          Self-taught Software Engineer<span></span>Code Enthusiast
-        </h2>
-        <Link to="/contact" className="flat-button">
-          CONTACT ME
-        </Link>
+    <div className="container">
+      <div className="page-container home-page">
+        <div className="text-zone">
+          <h1>
+            <span>
+              Hi!
+              <br />
+              I'm <Link className="name" to="/about">Kevin</Link>
+              <br />
+              <span className="title">Full-Stack Developer</span>
+            </span>
+          </h1>
+          <h2>
+            Self-taught Software Engineer<span></span>Code Enthusiast
+          </h2>
+          <Link to="/contact" className="flat-button">
+            CONTACT ME
+          </Link>
+        </div>
+        <Projects />
       </div>
-      <Projects />
     </div>
+    
   );
 };
 export default Home;
